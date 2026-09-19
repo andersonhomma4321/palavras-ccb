a6fd517e0c3fe16b5f36dc5c6d5059fad993f814
andersonhomma4321 <acy.h@hotmail.com>
2026-09-19 09:51:05 -0300

    atual5

--- data/videos.js
+++ data/videos.js
@@ -1,6 +1,6 @@
 export const listaVideos = [
   {
     title: "2026-09-16 - Palavra (Vídeo) - QUA 19_30 - II Coríntios 4 - IrLuiz",
-    youtubeId: "xU8ZurWedi8"
+    youtubeld: "xU8ZurWedi8"
   }
 ];
