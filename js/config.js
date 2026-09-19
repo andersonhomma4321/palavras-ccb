78ebc520586ff30f49ec2e831c3815f5edb2d421
andersonhomma4321 <acy.h@hotmail.com>
2026-09-19 09:42:32 -0300

    atual3

--- js/config.js
+++ js/config.js
@@ -4,17 +4,3 @@ export const CONFIG = {
   GITHUB_FILE: "data/videos.js", // Atualizado para apontar para o arquivo de vídeos
   YOUTUBE_EMBED_URL: "https://www.youtube.com/embed/"
 };
-
-
-/* export const CONFIG = {
-
-  GITHUB_OWNER: "andersonhomma4321",
-
-  GITHUB_REPO: "palavras-ccb",
-
-  GITHUB_FILE: "index.html",
-
-  YOUTUBE_EMBED_URL:
-    "https://www.youtube.com/embed/"
-
-}; */
