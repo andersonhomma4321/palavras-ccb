0ae0c3ff3d58009d309668313c0d3cefe7ce099b
andersonhomma4321 <acy.h@hotmail.com>
2026-09-20 22:29:14 -0300

    Removido vídeo: 2026-08-30 - Palavra (Vídeo) – DOM 10_00 - Mateus 6 - IrSalvador

--- data/videos.js
+++ data/videos.js
@@ -1,4 +1,3 @@
 export const listaVideos = [
-  { title: "2026 09 16   Palavra Vídeo –  QUA 19 30   II Coríntios 4   IrLuiz", youtubeId: "xU8ZurWedi8" },
-  { title: "2026-08-30 - Palavra (Vídeo) – DOM 10_00 - Mateus 6 - IrSalvador", youtubeId: "hKZVuR5Q1Wo" }
+  { title: "2026 09 16   Palavra Vídeo –  QUA 19 30   II Coríntios 4   IrLuiz", youtubeId: "xU8ZurWedi8" }
 ];
