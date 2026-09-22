01c52799b24a00d013acfa50a4da763ca33f25d9
andersonhomma4321 <acy.h@hotmail.com>
2026-09-22 09:08:09 -0300

    Adicionado vídeo: 2026 08 30   Palavra Vídeo – DOM 10 00   Mateus 6   IrSalvador

--- data/videos.js
+++ data/videos.js
@@ -1,3 +1,4 @@
 export const listaVideos = [
-  { title: "2026 09 16   Palavra Vídeo –  QUA 19 30   II Coríntios 4   IrLuiz", youtubeId: "xU8ZurWedi8" }
+  { title: "2026 09 16   Palavra Vídeo –  QUA 19 30   II Coríntios 4   IrLuiz", youtubeId: "xU8ZurWedi8" },
+  { title: "2026 08 30   Palavra Vídeo – DOM 10 00   Mateus 6   IrSalvador", youtubeId: "P88slFKzps8" }
 ];
