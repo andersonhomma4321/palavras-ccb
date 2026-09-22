990c1792deec32bee3fc147ee9ac2785ca6896d2
andersonhomma4321 <acy.h@hotmail.com>
2026-09-22 09:08:49 -0300

    Adicionado vídeo: 2026 08 26   Palavra Vídeo – QUA 19 30   João 12 12   IrLuiz

--- data/videos.js
+++ data/videos.js
@@ -1,4 +1,5 @@
 export const listaVideos = [
   { title: "2026 09 16   Palavra Vídeo –  QUA 19 30   II Coríntios 4   IrLuiz", youtubeId: "xU8ZurWedi8" },
-  { title: "2026 08 30   Palavra Vídeo – DOM 10 00   Mateus 6   IrSalvador", youtubeId: "P88slFKzps8" }
+  { title: "2026 08 30   Palavra Vídeo – DOM 10 00   Mateus 6   IrSalvador", youtubeId: "P88slFKzps8" },
+  { title: "2026 08 26   Palavra Vídeo – QUA 19 30   João 12 12   IrLuiz", youtubeId: "rkJIUn2SHFQ" }
 ];
