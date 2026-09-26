cbfed5428beb33c58b697a814208e8575e389987
andersonhomma4321 <acy.h@hotmail.com>
2026-09-26 12:56:55 -0300

    Adicionado vídeo: 2026 08 02   Palavra Vídeo – DOM 10 00   Isaías 55   IrSalvador

--- data/videos.js
+++ data/videos.js
@@ -7,5 +7,6 @@ export const listaVideos = [
   { title: "2026 08 16   Palavra Vídeo – DOM 10 00   João 20   IrLuiz", youtubeId: "cl8Hxmn92NE" },
   { title: "2026 08 12   Palavra Vídeo – QUA 19 30   Mateus 13   IrLuiz", youtubeId: "IgdU-crHS0c" },
   { title: "2026 08 09   Palavra Vídeo – DOM 10 00   Habacuque 3   IrSalvador", youtubeId: "c_L6dBTzXnA" },
-  { title: "2026 08 05   Palavra Vídeo – QUA 19 30   Lucas 16   IrLuiz", youtubeId: "KUHUdnccE7c" }
+  { title: "2026 08 05   Palavra Vídeo – QUA 19 30   Lucas 16   IrLuiz", youtubeId: "KUHUdnccE7c" },
+  { title: "2026 08 02   Palavra Vídeo – DOM 10 00   Isaías 55   IrSalvador", youtubeId: "p6CAJxv_Ym4" }
 ];
