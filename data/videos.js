7740fef380de1d7c15561a5a63b5c76427e70313
andersonhomma4321 <acy.h@hotmail.com>
2026-09-26 12:55:38 -0300

    Adicionado vídeo: 2026 08 09   Palavra Vídeo – DOM 10 00   Habacuque 3   IrSalvador

--- data/videos.js
+++ data/videos.js
@@ -5,5 +5,6 @@ export const listaVideos = [
   { title: "2026 08 23   Palavra Vídeo – DOM 10 00   Mateus 13 44   IrSalvador", youtubeId: "pqIxu_CRiDs" },
   { title: "2026 08 19   Palavra Vídeo – QUA 19 30   João 8 12   IrLuiz", youtubeId: "YBzi3VTmmSc" },
   { title: "2026 08 16   Palavra Vídeo – DOM 10 00   João 20   IrLuiz", youtubeId: "cl8Hxmn92NE" },
-  { title: "2026 08 12   Palavra Vídeo – QUA 19 30   Mateus 13   IrLuiz", youtubeId: "IgdU-crHS0c" }
+  { title: "2026 08 12   Palavra Vídeo – QUA 19 30   Mateus 13   IrLuiz", youtubeId: "IgdU-crHS0c" },
+  { title: "2026 08 09   Palavra Vídeo – DOM 10 00   Habacuque 3   IrSalvador", youtubeId: "c_L6dBTzXnA" }
 ];
